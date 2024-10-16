6de70bc69b372e84d42f6fc0623956d70ebd4bf8
Mustafacco <79732789+mustafacco7@users.noreply.github.com>
2024-10-16 06:30:31 +0300

    Initialize dlaliberte-textarea

---
+++ api/approvals_api.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import logging
+import re
+
+from framework import basehandlers
+from framework import permissions
+from internals import approval_defs
+from internals import models
+
+
+class ApprovalsAPI(basehandlers.APIHandler):
+  """Users may see the set of approvals on a feature, and add their own,
+  if allowed."""
+
+  def do_get(self, feature_id, field_id=None):
+    """Return a list of all approval values on the given feature."""
+    # Note: We assume that anyone may view approvals.
+    approvals = models.Approval.get_approvals(
+        feature_id=feature_id, field_id=field_id)
+    dicts = [av.format_for_template(add_id=False) for av in approvals]
+    data = {
+        'approvals': dicts,
+        }
+    return data
+
+  def do_post(self, feature_id=None):
+    """Set an approval value for the specified feature."""
+    field_id = self.get_int_param('fieldId')
+    new_state = self.get_int_param(
+        'state', validator=models.Approval.is_valid_state)
+    feature = self.get_specified_feature(feature_id=feature_id)
+    feature_id = feature.key.integer_id()
+    user = self.get_current_user(required=True)
+
+    approvers = approval_defs.get_approvers(field_id)
+    if not permissions.can_approve_feature(user, feature, approvers):
+      self.abort(403, msg='User is not an approver')
+
+    models.Approval.set_approval(
+        feature_id, field_id, new_state, user.email())
+
+    all_approval_values = models.Approval.get_approvals(
+        feature_id=feature_id, field_id=field_id)
+    if approval_defs.is_resolved(all_approval_values, field_id):
+      models.Approval.clear_request(feature_id, field_id)
+
+    # Callers don't use the JSON response for this API call.
+    return {'message': 'Done'}
+
+
+class ApprovalConfigsAPI(basehandlers.APIHandler):
+  """Get and set the approval configs for a feature."""
+
+  def do_get(self, feature_id):
+    """Return a list of all approval configs on the given feature."""
+    # Note: We assume that anyone may view approval configs.
+    configs = models.ApprovalConfig.get_configs(feature_id)
+    dicts = [ac.format_for_template(add_id=False) for ac in configs]
+    possible_owners_by_field = {
+        field_id: approval_defs.get_approvers(field_id)
+        for field_id in approval_defs.APPROVAL_FIELDS_BY_ID
+    }
+    data = {
+        'configs': dicts,
+        'possible_owners': possible_owners_by_field,
+        }
+    return data
+
+  def do_post(self, feature_id):
+    """Set an approval config for the specified feature."""
+    field_id = self.get_int_param('fieldId')
+    owners_str = self.get_param('owners')
+    next_action_str = self.get_param('nextAction')
+    additional_review = self.get_param('additionalReview')
+    feature = self.get_specified_feature(feature_id=feature_id)
+    user = self.get_current_user(required=True)
+
+    # A user can set the config iff they could approve.
+    approvers = approval_defs.get_approvers(field_id)
+    if not permissions.can_approve_feature(user, feature, approvers):
+      self.abort(403, msg='User is not an approver')
+
+    owners = None
+    if owners_str is not None:
+      owners = [addr.strip() for addr in re.split(',', owners_str)
+                if addr.strip()]
+
+    next_action = None
+    if next_action_str:
+      try:
+        next_action = datetime.date.fromisoformat(next_action_str)
+      except ValueError:
+        self.abort(400, msg='Invalid date formate or value')
+
+    models.ApprovalConfig.set_config(
+        feature_id, field_id, owners, next_action, additional_review)
+
+    # Callers don't use the JSON response for this API call.
+    return {'message': 'Done'}
---
+++ static/components.js
@@ -0,0 +1,44 @@
+/** This is the entry file for rollup. It bundles all the web components: polymer-paper components and our own components */
+
+// polymer components
+import '@polymer/iron-collapse';
+import '@polymer/iron-icon';
+import '@polymer/iron-iconset-svg';
+
+// Shoelace components
+// css is imported via _base.html in base.css, built by gulpfile.babel.js.
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
+import '@shoelace-style/shoelace/dist/components/details/details.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+
+// Set the base path to the folder you copied Shoelace's assets to
+setBasePath('@shoelace-style/shoelace'); // /dist/assets
+
+// chromedash components
+import './elements/icons';
+import './elements/chromedash-approvals-dialog';
+import './elements/chromedash-banner';
+import './elements/chromedash-callout';
+import './elements/chromedash-color-status';
+import './elements/chromedash-feature';
+import './elements/chromedash-feature-detail';
+import './elements/chromedash-feature-filter';
+import './elements/chromedash-feature-table';
+import './elements/chromedash-featurelist';
+import './elements/chromedash-new-feature-list';
+import './elements/chromedash-gantt';
+import './elements/chromedash-legend';
+import './elements/chromedash-metadata';
+import './elements/chromedash-metrics';
+import './elements/chromedash-myfeatures';
+import './elements/chromedash-process-overview';
+import './elements/chromedash-textarea';
+import './elements/chromedash-timeline';
+import './elements/chromedash-toast';
+import './elements/chromedash-roadmap';
+import './elements/chromedash-roadmap-milestone-card';
+import './elements/chromedash-userlist';
+import './elements/chromedash-x-meter';
