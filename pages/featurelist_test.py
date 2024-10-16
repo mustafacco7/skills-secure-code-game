bb461409a1f9cb403405bd7bacc79ef50e2940df
Mustafacco <79732789+mustafacco7@users.noreply.github.com>
2024-10-16 06:30:33 +0300

    Initialize dlaliberte-select2

---
+++ pages/featurelist_test.py
@@ -0,0 +1,172 @@
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+
+import testing_config  # Must be imported first
+
+import os
+import flask
+import werkzeug
+import html5lib
+
+from framework import ramcache
+from internals import models
+from pages import featurelist
+
+test_app = flask.Flask(__name__)
+
+
+class TestWithFeature(testing_config.CustomTestCase):
+
+  REQUEST_PATH_FORMAT = 'subclasses fill this in'
+  HANDLER_CLASS = 'subclasses fill this in'
+
+  def setUp(self):
+    self.feature_1 = models.Feature(
+        name='feature one', summary='detailed sum', category=1, visibility=1,
+        standardization=1, web_dev_views=1, impl_status_chrome=1,
+        intent_stage=models.INTENT_IMPLEMENT)
+    self.feature_1.put()
+    self.feature_id = self.feature_1.key.integer_id()
+
+    self.request_path = self.REQUEST_PATH_FORMAT % {
+        'feature_id': self.feature_id,
+    }
+    self.handler = self.HANDLER_CLASS()
+
+  def tearDown(self):
+    self.feature_1.key.delete()
+    ramcache.flush_all()
+    ramcache.check_for_distributed_invalidation()
+
+
+class FeaturesJsonHandlerTest(TestWithFeature):
+
+  REQUEST_PATH_FORMAT = '/features.json'
+  HANDLER_CLASS = featurelist.FeaturesJsonHandler
+
+  def test_get_template_data(self):
+    """User can get a JSON feed of all features."""
+    testing_config.sign_in('user@example.com', 111)
+    with test_app.test_request_context(self.request_path):
+      json_data = self.handler.get_template_data()
+
+    self.assertEqual(1, len(json_data))
+    self.assertEqual('feature one', json_data[0]['name'])
+
+  def test_get_template_data__unlisted_no_perms(self):
+    """JSON feed does not include unlisted features for users who can't edit."""
+    self.feature_1.unlisted = True
+    self.feature_1.put()
+
+    testing_config.sign_out()
+    with test_app.test_request_context(self.request_path):
+      json_data = self.handler.get_template_data()
+    self.assertEqual(0, len(json_data))
+
+    testing_config.sign_in('user@example.com', 111)
+    with test_app.test_request_context(self.request_path):
+      json_data = self.handler.get_template_data()
+    self.assertEqual(0, len(json_data))
+
+  def test_get_template_data__unlisted_can_edit(self):
+    """JSON feed includes unlisted features for users who may edit."""
+    self.feature_1.unlisted = True
+    self.feature_1.put()
+
+    testing_config.sign_in('user@google.com', 111)
+    with test_app.test_request_context(self.request_path):
+      json_data = self.handler.get_template_data()
+    self.assertEqual(1, len(json_data))
+    self.assertEqual('feature one', json_data[0]['name'])
+
+
+class FeatureListHandlerTest(TestWithFeature):
+
+  REQUEST_PATH_FORMAT = '/features'
+  HANDLER_CLASS = featurelist.FeatureListHandler
+
+  def test_get_template_data(self):
+    """User can get a feature list page."""
+    with test_app.test_request_context(self.request_path):
+      template_data = self.handler.get_template_data()
+
+    self.assertIn('IMPLEMENTATION_STATUSES', template_data)
+
+
+class FeatureListTemplateTest(TestWithFeature):
+
+  HANDLER_CLASS = featurelist.FeatureListHandler
+
+  def setUp(self):
+    super(FeatureListTemplateTest, self).setUp()
+    with test_app.test_request_context(self.request_path):
+      self.template_data = self.handler.get_template_data(
+          feature_id=self.feature_id)
+
+      self.template_data.update(self.handler.get_common_data())
+      self.template_data['nonce'] = 'fake nonce'
+      template_path = self.handler.get_template_path(self.template_data)
+      self.full_template_path = os.path.join(template_path)
+    
+  def test_html_rendering(self):
+    """We can render the template with valid html."""
+    template_text = self.handler.render(
+        self.template_data, self.full_template_path)
+    parser = html5lib.HTMLParser(strict=True)
+    document = parser.parse(template_text)
+
+
+class FeatureListXMLHandlerTest(TestWithFeature):
+
+  REQUEST_PATH_FORMAT = '/features.xml'
+  HANDLER_CLASS = featurelist.FeatureListXMLHandler
+
+  def test_get_template_data__no_filters(self):
+    """User can get an XML feed of all features."""
+    with test_app.test_request_context(self.request_path):
+      actual_text, actual_headers = self.handler.get_template_data()
+
+    self.assertTrue(actual_text.startswith('<?xml'))
+    self.assertIn('feature one', actual_text)
+    self.assertIn('detailed sum', actual_text)
+    self.assertIn(str(self.feature_id), actual_text)
+
+    self.assertIn('atom+xml', actual_headers['Content-Type'])
+
+  def test_get_template_data__category(self):
+    """User can get an XML feed of features by category."""
+    request_path = self.request_path + '?category=web components'
+    with test_app.test_request_context(request_path):
+      actual_text, actual_headers = self.handler.get_template_data()
+
+    # It is an XML feed
+    self.assertTrue(actual_text.startswith('<?xml'))
+    self.assertIn('atom+xml', actual_headers['Content-Type'])
+    self.assertIn('Features', actual_text)
+
+    # feature_1 is in the list
+    self.assertIn('feature one', actual_text)
+    self.assertIn('detailed sum', actual_text)
+    self.assertIn(str(self.feature_id), actual_text)
+
+
+    request_path = self.request_path + '?category=css'
+    with test_app.test_request_context(request_path):
+      actual_text, actual_headers = self.handler.get_template_data()
+
+    self.assertTrue(actual_text.startswith('<?xml'))
+    self.assertNotIn('feature one', actual_text)
