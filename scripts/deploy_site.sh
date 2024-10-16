40ca44b3e54e4e41542c2db4574dfcfc0fa4dd63
Mustafacco <79732789+mustafacco7@users.noreply.github.com>
2024-10-16 06:30:29 +0300

    Initialize dlaliberte-polymer

---
+++ scripts/deploy_site.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Deploys the app to App Engine.
+#
+# Note: This script should be used in place of using appcfg.py update directly
+# to update the application on App Engine.
+#
+# Copyright 2015 Eric Bidelman <ericbidelman@chromium.org>
+
+
+deployVersion=$1
+appName=${2:-cr-status}
+usage="Usage: deploy.sh `date +%Y-%m-%d`"
+
+if [ -z "$deployVersion" ]
+then
+  echo "App version not specified."
+  echo $usage
+  exit 0
+fi
+
+# The directory in which this script resides.
+readonly BASEDIR=$(dirname $BASH_SOURCE)
+
+gulp
+
+gcloud app deploy \
+  --project $appName \
+  --version $deployVersion \
+  --no-promote \
+  $BASEDIR/../py2/app-py2.yaml \
+  $BASEDIR/../notifier.yaml \
+  $BASEDIR/../app-py3.yaml  \
+  $BASEDIR/../dispatch.yaml \
+  $BASEDIR/../cron.yaml
