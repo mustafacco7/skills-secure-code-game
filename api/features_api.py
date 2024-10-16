2a952e678462a5052ebbd5652a4cc611c3b4f9f3
Mustafacco <79732789+mustafacco7@users.noreply.github.com>
2024-10-16 06:31:22 +0300

    Initialize 20230611-enhance-link-frontend

---
+++ api/features_api.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import datetime
+from typing import Any
+from google.cloud import ndb
+
+from api import api_specs
+from api import converters
+from framework import basehandlers
+from framework import permissions
+from framework import rediscache
+from framework import users
+from internals.core_enums import *
+from internals.core_models import FeatureEntry, MilestoneSet, Stage
+from internals.review_models import Gate
+from internals.data_types import VerboseFeatureDict
+from internals import feature_helpers
+from internals import search
+import settings
+
+
+class FeaturesAPI(basehandlers.APIHandler):
+  """Features are the the main records that we track."""
+
+  def _abort_invalid_data_type(
+      self, field: str, field_type: str, value: Any) -> None:
+    """Abort the process if an invalid data type is given."""
+    self.abort(400, msg=(
+        f'Bad value for field {field} of type {field_type}: {value}'))
+
+  def _format_field_val(
+      self,
+      field: str,
+      field_type: str,
+      value: Any,
+    ) -> str | int | bool | list | None:
+    """Format the given feature value based on the field type."""
+
+    # If the field is empty, no need to format.
+    if value is None:
+      return None
+
+    # TODO(DanielRyanSmith): Write checks to ensure enum values are valid.
+    if (field_type == 'list'):
+      if field == 'blink_components' and len(value) == 0:
+        return [settings.DEFAULT_COMPONENT]
+      return value
+    elif field_type == 'int':
+      try:
+        return int(value)
+      except ValueError:
+        self._abort_invalid_data_type(field, field_type, value)
+    elif field_type == 'bool':
+      return bool(value)
+    return str(value)
+
+  def get_one_feature(self, feature_id: int) -> VerboseFeatureDict:
+    feature = FeatureEntry.get_by_id(feature_id)
+    if not feature:
+      self.abort(404, msg='Feature %r not found' % feature_id)
+    user = users.get_current_user()
+    if feature.deleted and not permissions.can_edit_feature(user, feature_id):
+      self.abort(404, msg='Feature %r not found' % feature_id)
+    return converters.feature_entry_to_json_verbose(feature)
+
+  def do_search(self):
+    user = users.get_current_user()
+    # Show unlisted features to site editors or admins.
+    show_unlisted_features = permissions.can_edit_any_feature(user)
+    features_on_page = None
+
+    # Query-string parameter 'milestone' is provided
+    milestone = self.get_int_arg('milestone')
+    if milestone:
+      features_by_type = feature_helpers.get_in_milestone(
+        show_unlisted=show_unlisted_features, milestone=milestone)
+      total_count = sum(len(features_by_type[t]) for t in features_by_type)
+      return {
+          'features_by_type': features_by_type,
+          'total_count': total_count,
+          }
+
+    user_query = self.request.args.get('q', '')
+    sort_spec = self.request.args.get('sort')
+    num = self.get_int_arg('num', search.DEFAULT_RESULTS_PER_PAGE)
+    start = self.get_int_arg('start', 0)
+
+    show_enterprise = 'feature_type' in user_query
+    try:
+      features_on_page, total_count = search.process_query(
+          user_query, sort_spec=sort_spec, show_unlisted=show_unlisted_features,
+          show_enterprise=show_enterprise, start=start, num=num)
+    except ValueError as err:
+      self.abort(400, msg=str(err))
+
+    return {
+        'total_count': total_count,
+        'features': features_on_page,
+        }
+
+  def do_get(self, **kwargs):
+    """Handle GET requests for a single feature or a search."""
+    # TODO(danielrsmith): This request gives two independent return types
+    # based on whether a feature_id was specified. Determine the best
+    # way to handle this in a strictly-typed manner and implement it.
+    feature_id = kwargs.get('feature_id', None)
+    if feature_id:
+      return self.get_one_feature(feature_id)
+    return self.do_search()
+
+  @permissions.require_create_feature
+  def do_post(self, **kwargs):
+    """Handle POST requests to create a single feature."""
+    body = self.get_json_param_dict()
+
+    # A feature creation request should have all required fields.
+    for field in FeatureEntry.REQUIRED_FIELDS:
+      if field not in body:
+        self.abort(400, msg=f'Required field "{field}" not provided.')
+
+    fields_dict = {}
+    for field, field_type in api_specs.FEATURE_FIELD_DATA_TYPES:
+      if field in body:
+        fields_dict[field] = self._format_field_val(
+            field, field_type, body[field])
+
+    # Try to create the feature using the provided data.
+    try:
+      feature = FeatureEntry(**fields_dict,
+                             creator_email=self.get_current_user().email())
+      feature.put()
+    except Exception as e:
+      self.abort(400, msg=str(e))
+    id = feature.key.integer_id()
+
+    self._write_stages_and_gates_for_feature(id, feature.feature_type)
+    return {'message': f'Feature {id} created.',
+            'feature_id': id}
+
+  def _write_stages_and_gates_for_feature(
+      self, feature_id: int, feature_type: int) -> None:
+    """Write each Stage and Gate entity for newly created feature."""
+    # Obtain a list of stages and gates for the given feature type.
+    stages_gates = STAGES_AND_GATES_BY_FEATURE_TYPE[feature_type]
+
+    for stage_type, gate_types in stages_gates:
+      # Don't create a trial extension stage pre-emptively.
+      if stage_type == STAGE_TYPES_EXTEND_ORIGIN_TRIAL[feature_type]:
+        continue
+
+      stage = Stage(feature_id=feature_id, stage_type=stage_type)
+      stage.put()
+      new_gates: list[Gate] = []
+      # Stages can have zero or more gates.
+      for gate_type in gate_types:
+        gate = Gate(feature_id=feature_id, stage_id=stage.key.integer_id(),
+                    gate_type=gate_type, state=Gate.PREPARING)
+        new_gates.append(gate)
+
+      if new_gates:
+        ndb.put_multi(new_gates)
+
+  def _update_field_value(
+      self,
+      entity: FeatureEntry | MilestoneSet | Stage,
+      field: str,
+      field_type: str,
+      value: Any
+    ) -> None:
+    new_value = self._format_field_val(field, field_type, value)
+    setattr(entity, field, new_value)
+
+  def _patch_update_stages(
+      self,
+      stage_changes_list: list[dict[str, Any]]
+    ) -> bool:
+    """Update stage fields with changes provided in the PATCH request."""
+    stages: list[Stage] = []
+    for change_info in stage_changes_list:
+      stage_was_updated = False
+      # Check if valid ID is provided and fetch stage if it exists.
+      if 'id' not in change_info:
+        self.abort(400, msg='Missing stage ID in stage updates')
+      id = change_info['id']
+      stage = Stage.get_by_id(id)
+      if not stage:
+        self.abort(400, msg=f'Stage not found for ID {id}')
+
+      # Update stage fields.
+      for field, field_type in api_specs.STAGE_FIELD_DATA_TYPES:
+        if field not in change_info:
+          continue
+        self._update_field_value(stage, field, field_type, change_info[field])
+        stage_was_updated = True
+
+      # Update milestone fields.
+      milestones = stage.milestones
+      for field, field_type in api_specs.MILESTONESET_FIELD_DATA_TYPES:
+        if field not in change_info:
+          continue
+        if milestones is None:
+          milestones = MilestoneSet()
+        self._update_field_value(
+            milestones, field, field_type, change_info[field])
+        stage_was_updated = True
+      stage.milestones = milestones
+
+      if stage_was_updated:
+        stages.append(stage)
+
+    # Save all of the updates made.
+    # Return a boolean representing if any changes were made to any stages.
+    if stages:
+      ndb.put_multi(stages)
+      return True
+    return False
+
+  def _patch_update_special_fields(
+      self,
+      feature: FeatureEntry,
+      feature_changes: dict[str, Any],
+      has_updated: bool
+    ) -> None:
+    """Handle any special FeatureEntry fields."""
+    now = datetime.now()
+    # Handle accuracy notifications if this is an accuracy verification request.
+    if 'accurate_as_of' in feature_changes:
+      feature.accurate_as_of = now
+      feature.outstanding_notifications = 0
+      has_updated = True
+    
+    if has_updated:
+      user_email = self.get_current_user().email()
+      feature.updater_email = user_email
+      feature.updated = now
+
+  def _patch_update_feature(
+      self,
+      feature: FeatureEntry,
+      feature_changes: dict[str, Any],
+      has_updated: bool
+    ) -> None:
+    """Update feature fields with changes provided in the PATCH request."""
+    for field, field_type in api_specs.FEATURE_FIELD_DATA_TYPES:
+      if field not in feature_changes:
+        continue
+      self._update_field_value(
+          feature, field, field_type, feature_changes[field])
+      has_updated = True
+    
+    self._patch_update_special_fields(feature, feature_changes, has_updated)
+    feature.put()
+
+  def do_patch(self, **kwargs):
+    """Handle PATCH requests to update fields in a single feature."""
+    body = self.get_json_param_dict()
+
+    # Check if valid ID is provided and fetch feature if it exists.
+    if 'id' not in body['feature_changes']:
+      self.abort(400, msg='Missing feature ID in feature updates')
+    feature_id = body['feature_changes']['id']
+    feature: FeatureEntry | None = FeatureEntry.get_by_id(feature_id)
+    if not feature:
+      self.abort(400, msg=f'Feature not found for ID {feature_id}')
+
+    # Validate the user has edit permissions and redirect if needed.
+    redirect_resp = permissions.validate_feature_edit_permission(
+        self, feature_id)
+    if redirect_resp:
+      return redirect_resp
+
+    has_updated = self._patch_update_stages(body['stages'])
+    self._patch_update_feature(feature, body['feature_changes'], has_updated)
+
+    return {'message': f'Feature {feature_id} updated.'}
+
+  @permissions.require_admin_site
+  def do_delete(self, **kwargs) -> dict[str, str]:
+    """Delete the specified feature."""
+    # TODO(jrobbins): implement undelete UI.  For now, use cloud console.
+    feature_id = kwargs.get('feature_id', None)
+    feature = self.get_specified_feature(feature_id=feature_id)
+    if feature is None:
+      return {'message': 'ID does not match any feature.'}
+    feature.deleted = True
+    feature.put()
+    rediscache.delete_keys_with_prefix(FeatureEntry.feature_cache_prefix())
+
+    # Write for new FeatureEntry entity.
+    feature_entry: FeatureEntry | None = (
+        FeatureEntry.get_by_id(feature_id))
+    if feature_entry:
+      feature_entry.deleted = True
+      feature_entry.put()
+
+    return {'message': 'Done'}
---
+++ client-src/elements/chromedash-guide-editall-page.js
@@ -0,0 +1,364 @@
+import {LitElement, css, html, nothing} from 'lit';
+import {ref} from 'lit/directives/ref.js';
+import {
+  getStageValue,
+  showToastMessage,
+  flattenSections,
+  setupScrollToHash,
+  shouldShowDisplayNameField,
+  renderHTMLIf} from './utils.js';
+import './chromedash-form-table';
+import './chromedash-form-field';
+import {
+  formatFeatureForEdit,
+  FLAT_METADATA_FIELDS,
+  FLAT_ENTERPRISE_METADATA_FIELDS,
+  FORMS_BY_STAGE_TYPE,
+  FLAT_TRIAL_EXTENSION_FIELDS,
+} from './form-definition';
+import {SHARED_STYLES} from '../css/shared-css.js';
+import {FORM_STYLES} from '../css/forms-css.js';
+import {STAGE_SHORT_NAMES, STAGE_SPECIFIC_FIELDS, STAGE_ENT_ROLLOUT} from './form-field-enums.js';
+import {openAddStageDialog} from './chromedash-add-stage-dialog';
+
+
+export class ChromedashGuideEditallPage extends LitElement {
+  static get styles() {
+    return [
+      ...SHARED_STYLES,
+      ...FORM_STYLES,
+      css`
+      .enterprise-help-text > *, .enterprise-help-text li {
+        margin: revert;
+        padding: revert;
+        list-style: revert;
+      }
+    `];
+  }
+
+  static get properties() {
+    return {
+      featureId: {type: Number},
+      feature: {type: Object},
+      loading: {type: Boolean},
+      appTitle: {type: String},
+      nextPage: {type: String},
+      nextStageToCreateId: {type: Number},
+    };
+  }
+
+  constructor() {
+    super();
+    this.featureId = 0;
+    this.feature = {};
+    this.loading = true;
+    this.appTitle = '';
+    this.nextPage = '';
+    this.previousStageTypeRendered = 0;
+    this.sameTypeRendered = 0;
+    this.nextStageToCreateId = 0;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.loading = true;
+    Promise.all([
+      window.csClient.getFeature(this.featureId),
+    ]).then(([feature]) => {
+      this.feature = feature;
+      if (this.feature.name) {
+        document.title = `${this.feature.name} - ${this.appTitle}`;
+      }
+      this.loading = false;
+    }).catch(() => {
+      showToastMessage('Some errors occurred. Please refresh the page or try again later.');
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    document.title = this.appTitle;
+  }
+
+  /* Add the form's event listener after Shoelace event listeners are attached
+   * see more at https://github.com/GoogleChrome/chromium-dashboard/issues/2014 */
+  async registerHandlers(el) {
+    if (!el) return;
+    await el.updateComplete;
+
+    const hiddenTokenField = this.shadowRoot.querySelector('input[name=token]');
+    hiddenTokenField.form.addEventListener('submit', (event) => {
+      this.handleFormSubmit(event, hiddenTokenField);
+    });
+
+    setupScrollToHash(this);
+  }
+
+  handleFormSubmit(event, hiddenTokenField) {
+    event.preventDefault();
+
+    // get the XSRF token and update it if it's expired before submission
+    window.csClient.ensureTokenIsValid().then(() => {
+      hiddenTokenField.value = window.csClient.token;
+      event.target.submit();
+    });
+  }
+
+  handleCancelClick() {
+    window.location.href = `/guide/edit/${this.featureId}`;
+  }
+
+  renderSkeletons() {
+    return html`
+      <h3><sl-skeleton effect="sheen"></sl-skeleton></h3>
+      <section class="flat_form">
+        <h3><sl-skeleton effect="sheen"></sl-skeleton></h3>
+        <p>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+        </p>
+      </section>
+      <h3><sl-skeleton effect="sheen"></sl-skeleton></h3>
+      <section class="flat_form">
+        <h3><sl-skeleton effect="sheen"></sl-skeleton></h3>
+        <p>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+          <sl-skeleton effect="sheen"></sl-skeleton>
+        </p>
+      </section>
+    `;
+  }
+
+  getNextPage() {
+    return this.nextPage || this.feature.is_enterprise_feature ?
+    `/feature/${this.featureId}` : `/guide/edit/${this.featureId}`;
+  }
+
+  renderSubheader() {
+    return html`
+      <div id="subheader">
+        <h2 id="breadcrumbs">
+          <a href=${this.getNextPage()}>
+            <iron-icon icon="chromestatus:arrow-back"></iron-icon>
+            Edit feature: ${this.loading ? 'loading...' : this.feature.name}
+          </a>
+        </h2>
+      </div>
+    `;
+  }
+
+  getStageForm(stageType) {
+    return FORMS_BY_STAGE_TYPE[stageType] || null;
+  }
+
+  getHelpTextForStage(stageType) {
+    switch (stageType) {
+      case STAGE_ENT_ROLLOUT:
+        return html`
+        <section class="enterprise-help-text">
+          <h3>Rollout steps</h3>
+          <p>The enterprise release notes focus on changes to the stable channel.
+          Please add a stage for each milestone where something is changing on the stable channel.
+          For finch rollouts, use the milestone where the rollout starts.
+          </p>
+          <p>
+            For example, you may only have a single stage where you roll out to 100% of users on milestone N.
+          </p>
+          <p>A more complex example might look like this:</p>
+          <ul>
+            <li>On milestone N-1, you introduce a flag for early testing of an upcoming change, or start a deprecation origin trial</li>
+            <li>On milestone N, you start a finch rollout of a feature at 1% and introduce an enterprise policy for it</li>
+            <li>On milestone N+3, you remove the enterprise policy</li>
+          </ul>
+        </section>
+      `;
+      default:
+        return nothing;
+    }
+  }
+
+  renderStageSection(formattedFeature, sectionBaseName, feStage, stageFields) {
+    if (!stageFields) return nothing;
+
+    // Add a number differentiation if this stage type is the same as another stage.
+    let numberDifferentiation = '';
+    if (this.previousStageTypeRendered && this.previousStageTypeRendered === feStage.stage_type) {
+      this.sameTypeRendered += 1;
+      numberDifferentiation = ` ${this.sameTypeRendered}`;
+    } else {
+      this.previousStageTypeRendered = feStage.stage_type;
+      this.sameTypeRendered = 1;
+    }
+    let sectionName = `${sectionBaseName}${numberDifferentiation}`;
+    if (feStage.display_name) {
+      sectionName = `${sectionBaseName}: ${feStage.display_name} `;
+    }
+    const formFieldEls = stageFields.map(field => {
+      // Only show "display name" field if there is more than one stage of the same type.
+      if (field === 'display_name' &&
+          !shouldShowDisplayNameField(this.feature.stages, feStage.stage_type)) {
+        return nothing;
+      }
+      let value = formattedFeature[field];
+      if (STAGE_SPECIFIC_FIELDS.has(field)) {
+        value = getStageValue(feStage, field);
+      } else if (this.sameTypeRendered > 1) {
+        // Don't render fields that are not stage-specific if this is
+        // a stage type that is already being rendered.
+        // This is to avoid repeated fields on the edit-all page.
+        return nothing;
+      }
+      return html`
+        <chromedash-form-field
+          name=${field}
+          stageId=${feStage.id}
+          stageType=${feStage.to_create ? feStage.stage_type : undefined}
+          value=${value}
+          ?forEnterprise=${formattedFeature.is_enterprise_feature}>
+        </chromedash-form-field>
+      `;
+    });
+    const id = `${STAGE_SHORT_NAMES[feStage.stage_type] || 'metadata'}${this.sameTypeRendered}`
+      .toLowerCase();
+    const isEnterpriseFeatureRollout = formattedFeature.is_enterprise_feature &&
+      feStage.stage_type === STAGE_ENT_ROLLOUT;
+    return html`
+    ${renderHTMLIf(!isEnterpriseFeatureRollout, html`<h3 id="${id}">${sectionName}</h3>`)}
+    <section class="flat_form">
+      ${formFieldEls}
+    </section>
+    `;
+  }
+
+  /**
+   * Builds the HTML elements for rendering the form sections.
+   * @param {Object} formattedFeature Object describing the feature.
+   * @param {Array} feStages List of stages associated with the feature.
+   *
+   * @return {Array} allFormFields, an array of strings representing all the field
+   *   names that will exist on the page.
+   * @return {Array} formsToRender, All HTML elements to render in the form.
+   */
+  getForms(formattedFeature, feStages) {
+    // All features display the metadata section.
+    let fieldsOnly = flattenSections(formattedFeature.is_enterprise_feature ?
+      FLAT_ENTERPRISE_METADATA_FIELDS :
+      FLAT_METADATA_FIELDS);
+    const formsToRender = [
+      this.renderStageSection(formattedFeature, FLAT_METADATA_FIELDS.name, {}, fieldsOnly)];
+
+    // Generate a single array with the name of every field that is displayed.
+    let allFormFields = [...fieldsOnly];
+
+
+    let previousStageType = null;
+    for (const feStage of feStages) {
+      const stageForm = this.getStageForm(feStage.stage_type);
+      if (!stageForm) {
+        continue;
+      }
+
+      if (formattedFeature.is_enterprise_feature &&
+          feStage.stage_type !== previousStageType) {
+        formsToRender.push(this.getHelpTextForStage(feStage.stage_type));
+        previousStageType = feStage.stage_type;
+      }
+
+      fieldsOnly = flattenSections(stageForm);
+      formsToRender.push(this.renderStageSection(
+        formattedFeature, stageForm.name, feStage, fieldsOnly));
+      allFormFields = [...allFormFields, ...fieldsOnly];
+
+      // If extension stages are associated with this stage,
+      // render them in a separate section as well.
+      const extensions = feStage.extensions || [];
+      extensions.forEach(extensionStage => {
+        fieldsOnly = flattenSections(FLAT_TRIAL_EXTENSION_FIELDS);
+        let sectionName = FLAT_TRIAL_EXTENSION_FIELDS.name;
+        if (feStage.display_name) {
+          sectionName = ` ${FLAT_TRIAL_EXTENSION_FIELDS.name}: ${feStage.display_name} `;
+        }
+        formsToRender.push(this.renderStageSection(
+          formattedFeature,
+          sectionName,
+          extensionStage,
+          fieldsOnly));
+        allFormFields = [...allFormFields, ...fieldsOnly];
+      });
+    }
+
+    return [allFormFields, formsToRender];
+  }
+
+  getAllStageIds() {
+    const stageIds = [];
+    this.feature.stages.forEach(feStage => {
+      if (feStage.to_create) return;
+      stageIds.push(feStage.id);
+      // Check if any trial extension exist, and collect their IDs as well.
+      const extensions = feStage.extensions || [];
+      extensions.forEach(extensionStage => stageIds.push(extensionStage.id));
+    });
+    return stageIds.join(',');
+  }
+
+  renderAddStageButton() {
+    const text = this.feature.is_enterprise_feature ? 'Add Step': 'Add Stage';
+    return html`
+    <sl-button size="small" @click="${
+        () => openAddStageDialog(this.feature.id, this.feature.feature_type_int, this.AddNewStageToCreate.bind(this))}">
+      ${text}
+    </sl-button>`;
+  }
+
+  AddNewStageToCreate(stageType) {
+    this.feature.stages.push({
+      ...stageType,
+      to_create: true,
+      id: ++this.nextStageToCreateId});
+    this.feature.stages = this.feature.stages
+      .sort((a, b) => a.stage_type_int - b.stage_type_int);
+    this.feature = {...this.feature};
+  }
+
+  renderForm() {
+    const formattedFeature = formatFeatureForEdit(this.feature);
+    const stageIds = this.getAllStageIds();
+    const [allFormFields, formsToRender] = this.getForms(formattedFeature, this.feature.stages);
+    return html`
+      <form name="feature_form" method="POST" action="/guide/editall/${this.featureId}">
+        <input type="hidden" name="stages" value="${stageIds}">
+        <input type="hidden" name="token">
+        <input type="hidden" name="nextPage" value=${this.getNextPage()} >
+        <input type="hidden" name="form_fields" value=${allFormFields.join(',')}>
+        <chromedash-form-table ${ref(this.registerHandlers)}>
+          ${formsToRender}
+        </chromedash-form-table>
+        ${this.renderAddStageButton()}
+
+        <section class="final_buttons">
+          <input class="button" type="submit" value="Submit">
+          <button id="cancel-button" type="reset"
+            @click=${this.handleCancelClick}>Cancel</button>
+        </section>
+      </form>
+    `;
+  }
+
+  render() {
+    return html`
+      ${this.renderSubheader()}
+      ${this.loading ? this.renderSkeletons() : this.renderForm()}
+    `;
+  }
+}
+
+customElements.define('chromedash-guide-editall-page', ChromedashGuideEditallPage);
